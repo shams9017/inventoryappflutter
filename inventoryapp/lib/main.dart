3812b59a1df18a53f1044d60a037c90b94d46eaf
shams9017 <59844179+shams9017@users.noreply.github.com>
2021-01-29 23:10:53 -0600

    fixed the API load issue

--- inventoryapp/lib/main.dart
+++ inventoryapp/lib/main.dart
@@ -42,56 +42,40 @@ class _MyHomePageState extends State<MyHomePage> {
   List<String> item_names = [];
   String low_item_name;
   int total_count = 0;
-  int total_value;
+  int total_value = 0;
 
-  // get products method
-  Future<List<Product>> getProducts() async {
-    final http.Response response = await http
-        .get('https://inventorywebapishams.azurewebsites.net/product');
-
-    if (response.statusCode == 200) {
-      // If the call to the server was successful, parse the JSON
-      List<dynamic> values = <dynamic>[];
-      values = json.decode(response.body);
-
-      if (values.length > 0) {
-        //value is the number of records in database
-        for (int i = 0; i < values.length; i++) {
-          if (values[i] != null) {
-            Map<String, dynamic> map = values[i];
-            _productList.add(Product.fromJson(map));
-            item_names.add(map['name']);
-            low_item_count.add(map['count']);
-            total_count += map['count'];
-
-            total_value = map['unitPrice'] * map['count'];
-            // add the data for the chart
-            data.add(ItemCountSeries(
-                item: map['name'],
-                count: map['count'],
-                barColor: charts.ColorUtil.fromDartColor(Colors.blue)));
-          }
-        }
-
-        // check low item
-        for (int j = 0; j < low_item_count.length; j++) {
-          if (low_item_count[j] < 4) {
-            low_item_name = item_names[j];
-            showNotification(context);
-          }
-        }
-      }
-
-      return _productList;
-    } else {
-      // If that call was not successful, throw an error.
-      throw Exception('Failed to load');
-    }
-  }
 
   @override
   void initState() {
-    getProducts();
+    super.initState();
+    //getProducts();
+    APIService.getProducts().then((products) {
+      setState(() {
+        _productList = products;
+        print(_productList.length);
+        for (int j = 0; j < _productList.length; j++) {
+
+          int current_item_count = _productList[j].count;
+          int current_item_unitprice = _productList[j].unitPrice;
+          int current_item_value = current_item_count * current_item_unitprice;
+          total_count = total_count + current_item_count;
+          total_value = total_value + current_item_value;
+          low_item_count.add(_productList[j].count);
+
+          data.add(ItemCountSeries(
+              item: _productList[j].name,
+              count: _productList[j].count,
+              barColor: charts.ColorUtil.fromDartColor(Colors.blue)));
+          if (low_item_count[j] < 4) {
+            low_item_name = _productList[j].name;
+            showNotification(context);
+
+          }
+        }
+
+
+      });
+    });
   }
 
   @override
@@ -102,96 +86,96 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
           child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                  width: 500,
-                  padding: EdgeInsets.all(8),
-                  child: ItemCountChart(data: data)),
-            ),
-            Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                      width: 500,
+                      padding: EdgeInsets.all(8),
+                      child: ItemCountChart(data: data)),
                 ),
-                color: Colors.orange[400],
-                child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Container(
-                      width: 300,
-                      height: 80,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(total_count.toString(),
-                              style: TextStyle(
-                                  fontSize: 37.0, color: Colors.white)),
-                          Padding(
-                              padding: EdgeInsets.only(top: 10.0),
-                              child: Text("Total Items",
-                                  style: TextStyle(
-                                      fontSize: 20.0, color: Colors.white)))
-                        ],
-                      ),
-                    ))),
-            Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                color: Colors.green[400],
-                child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Container(
-                      width: 300,
-                      height: 80,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("\$" + total_value.toString(),
-                              style: TextStyle(
-                                  fontSize: 37.0, color: Colors.white)),
-                          Padding(
-                              padding: EdgeInsets.only(top: 10.0),
-                              child: Text("Total Value",
-                                  style: TextStyle(
-                                      fontSize: 20.0, color: Colors.white)))
-                        ],
-                      ),
-                    ))),
-            Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                color: Colors.red[400],
-                child: InkWell(
-                    onTap: () async {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductList()));
-                    },
+                Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: Colors.orange[400],
                     child: Padding(
                         padding: EdgeInsets.all(12.0),
                         child: Container(
                           width: 300,
-                          height: 25,
+                          height: 80,
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("See Products",
-                                  textAlign: TextAlign.center,
+                              Text(total_count.toString(),
                                   style: TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold)),
+                                      fontSize: 37.0, color: Colors.white)),
+                              Padding(
+                                  padding: EdgeInsets.only(top: 10.0),
+                                  child: Text("Total Items",
+                                      style: TextStyle(
+                                          fontSize: 20.0, color: Colors.white)))
                             ],
                           ),
-                        )))),
-          ],
-        ),
-      )),
+                        ))),
+                Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: Colors.green[400],
+                    child: Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Container(
+                          width: 300,
+                          height: 80,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("\$" + total_value.toString(),
+                                  style: TextStyle(
+                                      fontSize: 37.0, color: Colors.white)),
+                              Padding(
+                                  padding: EdgeInsets.only(top: 10.0),
+                                  child: Text("Total Value",
+                                      style: TextStyle(
+                                          fontSize: 20.0, color: Colors.white)))
+                            ],
+                          ),
+                        ))),
+                Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: Colors.red[400],
+                    child: InkWell(
+                        onTap: () async {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ProductList()));
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Container(
+                              width: 300,
+                              height: 25,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text("See Products",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold)),
+                                ],
+                              ),
+                            )))),
+              ],
+            ),
+          )),
     );
   }
 
@@ -200,16 +184,16 @@ class _MyHomePageState extends State<MyHomePage> {
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
-                title: Text("Low Item Notification"),
-                content:
-                    Text("Item named " + low_item_name + " is low in stock."),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text('Ok'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ]));
+            title: Text("Low Item Notification"),
+            content:
+            Text("Item named " + low_item_name + " is low in stock."),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ]));
   }
 }
